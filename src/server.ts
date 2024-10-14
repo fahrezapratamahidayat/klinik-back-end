73c3a8b18f6744b8a364b1558d8ec47d95b62b0e
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-10-14 22:06:03 +0700

    feat: update add get method for payment methods

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import bodyParser from 'body-parser';
 import { routes } from './routes';
 import { logger } from './utils/logger';
 import * as dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 const app: Application = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
 const envFile = `.env.${process.env.NODE_ENV || 'local'}`;
+const prisma = new PrismaClient();
 dotenv.config({ path: envFile });
 
 const PORT = process.env.PORT || 3000;
@@ -32,15 +34,24 @@ app.get('/token', async (req, res) => {
       }
     });
 
-    return res.status(200).json({ token: response.data });
+    res.status(200).json({ token: response.data });
   } catch (error: any) {
     console.error(error);
-    return res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/payment-methods', async (req, res) => {
+  try {
+    const paymentMethods = await prisma.paymentMethod.findMany();
+    res.json(paymentMethods);
+  } catch (error) {
+    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil metode pembayaran' });
   }
 });
 
 routes(app);
 
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT} in ${envFile} mode`);
+  logger.info(`Server running on port http://localhost:${PORT} in ${envFile} mode`);
 });
