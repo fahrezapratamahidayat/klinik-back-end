fd080aad4dae371fb5b7e1689ce6ede0f531cf05
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-10-14 22:11:55 +0700

    del : moving to new file

--- src/routes/patient-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createPatient, getPatient } from "../controllers/patient-controller";
-
-export const PatientRoutes: Router = Router();
-
-PatientRoutes.post('/', createPatient);
-PatientRoutes.get('/:nik', getPatient);
