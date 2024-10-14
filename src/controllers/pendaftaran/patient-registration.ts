4ffee0307cc61b4df80e00a4ef1ee8defad8b5bc
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-10-14 22:10:10 +0700

    feat: add patient registasion contoller

---
+++ src/controllers/pendaftaran/patient-registration.ts
@@ -0,0 +1,177 @@
+import { PrismaClient } from "@prisma/client";
+import e, { Request, Response } from "express";
+import { createPatientRegistrationValidation } from "../../validations/patient-validation";
+import { validateInput } from "../../helpers/validation";
+import { generateRegistrationId } from "../../utils/generate";
+
+const prisma = new PrismaClient();
+export const createPatientRegistration = async (
+  req: Request,
+  res: Response
+) => {
+  const { patientId, doctorId, paymentMethodId, roomId, scheduleId, isOnline } =
+    req.body;
+  if (!validateInput(createPatientRegistrationValidation, req, res)) return;
+
+  try {
+    // Periksa keberadaan pasien, dokter, dan metode pembayaran
+    const [patient, doctor, paymentMethod] = await Promise.all([
+      prisma.patient.findUnique({ where: { id: patientId } }),
+      prisma.doctor.findUnique({ where: { id: doctorId } }),
+      prisma.paymentMethod.findUnique({ where: { id: paymentMethodId } }),
+    ]);
+
+    if (!patient)
+      return res.status(404).json({ message: "Pasien tidak ditemukan" });
+    if (!doctor)
+      return res.status(404).json({ message: "Dokter tidak ditemukan" });
+    if (!paymentMethod)
+      return res
+        .status(404)
+        .json({ message: "Metode pembayaran tidak ditemukan" });
+
+    // Periksa jadwal dokter
+    const doctorSchedule = await prisma.doctorSchedule.findUnique({
+      where: { id: scheduleId },
+    });
+
+    if (!doctorSchedule || !doctorSchedule.isActive) {
+      return res.status(400).json({
+        status: false,
+        statusCode: 400,
+        message: "Jadwal dokter tidak tersedia",
+      });
+    }
+
+    // Validasi jadwal dokter untuk hari ini
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // 0 = Minggu, 1 = Senin, dst.
+
+    if (doctorSchedule.dayOfWeek !== dayOfWeek) {
+      return res.status(400).json({
+        status: false,
+        statusCode: 400,
+        message: "Jadwal dokter tidak tersedia untuk hari ini",
+      });
+    }
+
+    // Validasi waktu jadwal
+    const currentTime = new Date();
+    if (currentTime > doctorSchedule.endTime) {
+      return res.status(400).json({
+        status: false,
+        statusCode: 400,
+        message: "Jadwal dokter untuk hari ini sudah berakhir",
+      });
+    }
+
+    // Periksa kuota yang tersedia
+    const availableQuota = isOnline
+      ? doctorSchedule.remainingOnlineQuota
+      : doctorSchedule.remainingOfflineQuota;
+
+    if (availableQuota <= 0) {
+      return res.status(400).json({
+        status: false,
+        statusCode: 400,
+        message: `Kuota ${
+          isOnline ? "online" : "offline"
+        } untuk jadwal ini telah habis`,
+      });
+    }
+
+    // Buat registrasi pasien
+    const registrationNumber = await generateRegistrationId();
+    const patientRegistration = await prisma.patientRegistration.create({
+      data: {
+        patientId,
+        doctorId,
+        roomId,
+        paymentMethodId,
+        registrationNumber,
+        registrationDate: new Date(),
+      },
+    });
+
+    // Update kuota yang tersisa
+    await prisma.doctorSchedule.update({
+      where: { id: scheduleId },
+      data: {
+        remainingOnlineQuota: isOnline
+          ? doctorSchedule.remainingOnlineQuota - 1
+          : doctorSchedule.remainingOnlineQuota,
+        remainingOfflineQuota: !isOnline
+          ? doctorSchedule.remainingOfflineQuota - 1
+          : doctorSchedule.remainingOfflineQuota,
+      },
+    });
+
+    res.status(201).json({
+      status: true,
+      statusCode: 201,
+      message: "Registrasi pasien berhasil dibuat",
+      data: patientRegistration,
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getPatientRegistrations = async (req: Request, res: Response) => {
+  try {
+    const patientRegistrations = await prisma.patientRegistration.findMany();
+    if (patientRegistrations.length === 0)
+      return res.status(404).json({
+        status: false,
+        statusCode: 404,
+        message: "Patient registrations not found",
+        data: [],
+        total: 0,
+      });
+    res.status(200).json({
+      status: true,
+      statusCode: 200,
+      message: "Patient registrations found",
+      data: patientRegistrations,
+      total: patientRegistrations.length,
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getPatientRegistrationById = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+  try {
+    const patientRegistration = await prisma.patientRegistration.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        patient: true,
+        doctor: true,
+        room: true,
+        PaymentMethod: true,
+      },
+    });
+    if (!patientRegistration)
+      return res.status(404).json({
+        status: false,
+        statusCode: 404,
+        message: "Patient registration not found",
+        data: null,
+        total: 0,
+      });
+    res.status(200).json({
+      status: true,
+      statusCode: 200,
+      message: "Patient registration found",
+      data: patientRegistration,
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
