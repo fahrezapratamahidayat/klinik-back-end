36189d1be4b31fc44a54e878d70417f8de115af7
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-10-22 21:09:13 +0700

    update doctor validation

--- src/validations/doctor-validation.ts
+++ src/validations/doctor-validation.ts
@@ -55,38 +55,38 @@ export const createDoctorValidation = z.object({
 
 export const updateDoctorValidation = z
   .object({
-    name: z.string().min(1, "Nama harus diisi").optional(),
-    gender: z.nativeEnum(Gender).optional(),
+    name: z.string({required_error: "Nama harus diisi"}).min(1, "Nama harus diisi").optional(),
+    gender: z.nativeEnum(Gender, {required_error: "Jenis kelamin harus diisi"}).optional(),
     birthDate: z.coerce.date().optional(),
-    birthPlace: z.string().min(1, "Tempat lahir harus diisi").optional(),
-    nip: z.string().min(1, "NIP harus diisi"),
+    birthPlace: z.string({required_error: "Tempat lahir harus diisi"}).min(1, "Tempat lahir harus diisi").optional(),
+    nip: z.string({required_error: "NIP harus diisi"}).min(1, "NIP harus diisi"),
     nik: z.string().length(16, "NIK harus 16 digit").optional(),
-    sip: z.string().min(1, "SIP harus diisi").optional(),
-    str: z.string().min(1, "STR harus diisi").optional(),
-    specialization: z.string().min(1, "Spesialisasi harus diisi").optional(),
-    status: z.nativeEnum(DoctorStatus).optional(),
+    sip: z.string({required_error: "SIP harus diisi"}).min(1, "SIP harus diisi").optional(),
+    str: z.string({required_error: "STR harus diisi"}).min(1, "STR harus diisi").optional(),
+    specialization: z.string({required_error: "Spesialisasi harus diisi"}).min(1, "Spesialisasi harus diisi").optional(),
+    status: z.nativeEnum(DoctorStatus, {required_error: "Status harus diisi"}).optional(),
     address: z
       .object({
         use: z
           .string()
           .min(1, "bisa di isi dengan nilai rumah/kantor/lainnya")
           .optional(),
-        line: z.string().min(1, "Alamat harus diisi").optional(),
-        city: z.string().min(1, "Kota harus diisi").optional(),
-        postalCode: z.string().min(1, "Kode pos harus diisi").optional(),
-        country: z.string().min(1, "Negara harus diisi").optional(),
+        line: z.string({required_error: "Alamat harus diisi"}).min(1, "Alamat harus diisi").optional(),
+        city: z.string({required_error: "Kota harus diisi"}).min(1, "Kota harus diisi").optional(),
+        postalCode: z.string({required_error: "Kode pos harus diisi"}).min(1, "Kode pos harus diisi").optional(),
+        country: z.string({required_error: "Negara harus diisi"}).min(1, "Negara harus diisi").optional(),
         extension: z
           .object({
             provinceCode: z
-              .string()
+              .string({required_error: "Kode provinsi harus diisi"})
               .min(1, "Kode provinsi harus diisi")
               .optional(),
             districtCode: z
-              .string()
+              .string({required_error: "Kode kabupaten harus diisi"})
               .min(1, "Kode kabupaten harus diisi")
               .optional(),
             subdistrictCode: z
-              .string()
+              .string({required_error: "Kode kecamatan harus diisi"})
               .min(1, "Kode kecamatan harus diisi")
               .optional(),
             villageCode: z.string().min(1, "Kode desa harus diisi").optional(),
@@ -118,12 +118,38 @@ export const updateDoctorValidation = z
   .partial();
 
 export const createDoctorScheduleValidation = z.object({
-  doctorId: z.string(),
-  roomId: z.string(),
-  dayOfWeek: z.number().int().min(1).max(7),
-  startTime: z.string(),
-  endTime: z.string(),
-  isActive: z.boolean(),
-  offlineQuota: z.number().int().min(0),
-  onlineQuota: z.number().int().min(0),
-});
+  doctorId: z.string().uuid(),
+  roomId: z.string().uuid(),
+    schedules: z.array(
+      z.object({
+        date: z.coerce.date(),
+        day: z.enum([
+          "Senin",
+          "Selasa",
+          "Rabu",
+          "Kamis",
+          "Jumat",
+          "Sabtu",
+          "Minggu",
+        ]),
+        startTime: z.string().regex(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+          message: "Format waktu tidak valid",
+        }),
+        endTime: z.string().regex(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+          message: "Format waktu tidak valid",
+        }),
+        offlineQuota: z
+          .number({ required_error: "Kuota Offline harus diisi" })
+          .int({ message: "Kuota Offline harus berupa angka" })
+          .nonnegative({ message: "Kuota Offline harus berupa angka" }),
+        onlineQuota: z
+          .number({ required_error: "Kuota Online harus diisi" })
+          .int({ message: "Kuota Online harus berupa angka" })
+          .nonnegative({ message: "Kuota Online harus berupa angka" }),
+        totalQuota: z
+          .number({ required_error: "Total Kuota harus diisi" })
+          .int({ message: "Total Kuota harus berupa angka" })
+          .nonnegative({ message: "Total Kuota harus berupa angka" }),
+      })
+    ),
+  });
