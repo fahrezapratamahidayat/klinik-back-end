6cacc37a2d5174ed3222cbdc011b3c00bda8578f
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-12-19 13:01:08 +0700

    refactor: update Vercel configuration and simplify main route response
    - Changed the build source in vercel.json from './server.ts' to 'server.ts' for consistency.
    - Simplified the main route response in main-route.ts to return a plain string instead of a JSON object, enhancing clarity.

--- src/routes/main-route.ts
+++ src/routes/main-route.ts
@@ -3,10 +3,6 @@ import { Router } from "express";
 export const MainRouter: Router = Router();
 
 MainRouter.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Welcome to Klinik API",
-        version: "1.0.0",
-        author: "Klinik"
-    })
+    res.send("Welcome to Klinik API")
 });
 
